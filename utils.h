9d737bb11b189163a15e5972c0cba9b742244cef
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-16 21:48:18 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -3,9 +3,12 @@
 #include <iostream>
 namespace sdds {
    extern bool debug;
+   
    int getInt(const char* prompt = nullptr);
    int getInt(int min, int max, const char* prompt = nullptr, const char* errorMessage = nullptr, bool showRangeAtError = true);
+   
    char* getcstr(const char* prompt = nullptr, std::istream& istr = std::cin, char delimiter = '\n');
+   
    int getTime();
 }
 #endif // !SDDS_UTILS_H_
