4cfc3208366bbc6e61b187aec8fa0a4d6108ef5f
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-16 21:40:53 -0400

    Update utils.h

--- utils.h
+++ utils.h
@@ -2,11 +2,11 @@
 #define SDDS_UTILS_H_
 #include <iostream>
 namespace sdds {
-   extern bool debug; // making sdds::debug variable global to all the files
-                      // which include: "utils.h"
-   int getTime(); // returns the time of day in minutes
+   extern bool debug;
    int getInt(const char* prompt = nullptr);
    int getInt(int min, int max, const char* prompt = nullptr, const char* errorMessage = nullptr, bool showRangeAtError = true);
    char* getcstr(const char* prompt = nullptr, std::istream& istr = std::cin, char delimiter = '\n');
+   int getTime();
 }
 #endif // !SDDS_UTILS_H_
+
