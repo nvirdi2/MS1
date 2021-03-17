6fb99b705280cdd3b948c57fc0b8bb04655f1ad4
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-16 23:00:50 -0400

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -1,8 +1,16 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: March 16, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #include <iostream>
+#include <string>
+
 #include <cstring>
 #include <cstdlib>
 #include <ctime>
-#include <string>
 
 #include "utils.h"
 #include "Time.h"
@@ -11,109 +19,151 @@ using namespace std;
 namespace sdds
 {
     bool debug{false};
-    int getInt(const char *prompt)
-    {
-        string userEntry;
-        bool intFlag{true};
-        bool notInt;
-        bool voidInt;
-        unsigned i;
-        if (prompt != nullptr)
-            cout << prompt;
-        getline(cin, userEntry);
-        while (intFlag)
-        {
-            voidInt = false;
-            notInt = false;
-            if (!isdigit(userEntry.c_str()[0]))
-                voidInt = true;
-            for (i = 0; i < userEntry.length() && !voidInt && !notInt; i++)
-            {
-                if (!isdigit(userEntry.c_str()[i]) && i != 0)
-                    notInt = true;
-            }
-            if (voidInt)
-            {
-                cout << "Bad integer value, try again: ";
-                getline(cin, userEntry);
-            }
-            if (notInt)
-            {
-                cout << "Enter only an integer, try again: ";
-                getline(cin, userEntry);
-            }
-            if (!voidInt && !notInt)
-                intFlag = false;
-        }
 
-        return atoi(userEntry.c_str());
+    int getTime()
+    {
+        int minutes {-1};
+
+        if (debug)
+        {
+            Time x(0);
+
+            cout << "Enter current time: ";
+                do
+                {
+                    cin.clear();
+                    cin >> x;
+
+                        if (!cin)
+                        {
+                            cout << "Invlid time, try agian (HH:MM): ";
+                            cin.clear();
+                        }
+
+                            else
+                            {
+                                minutes = int(x);
+                            }
+                    cin.ignore(1000, '\n');
+
+                } while (minutes < 0);
+        }
+        
+        else
+        {
+            time_t x = time(NULL);
+
+            tm i = *localtime(&x);
+
+            minutes = i.tm_hour * 60 + i.tm_min;
+        } return minutes;
     }
 
-    int getInt(int min, int max, const char *prompt, const char *errorMessage, bool showRangeAtError)
+
+    int getInt(const char *prompt)
     {
-        int userInput = getInt(prompt);
-        bool flag{true};
-        
-        while (flag)
+        string UserInput;
+
+        unsigned x;
+
+        bool Flag{true};
+        bool notInteger;
+        bool Integerv;
+
+        if (prompt != nullptr)
         {
-            if (userInput <= max && userInput >= min)
-                flag = false;
-            else
-            {
-                if (errorMessage != nullptr)
-                    cout << errorMessage;
-                else
-                    getInt(prompt);
-                if (showRangeAtError)
-                    cout << "[" << min << " <= value <= " << max << "]: ";
-                userInput = getInt();
-            }
+            cout << prompt;
         }
-        return userInput;
+
+        getline(cin, UserInput);
+
+        while (Flag)
+        {
+            Integerv = false;
+            notInteger = false;
+
+            if (!isdigit(UserInput.c_str()[0]))
+            {
+                Integerv = true;
+            }
+
+                for (x = 0; x < UserInput.length() && !Integerv && !notInteger; x++)
+                {
+                    if (!isdigit(UserInput.c_str()[x]) && x != 0)
+                    {
+                        notInteger = true;
+                    }
+                }
+            
+                    if (Integerv)
+                    {
+                        cout << "Bad integer value, try again: ";
+                        getline(cin, UserInput);
+                    }
+            
+                        if (notInteger)
+                        {
+                            cout << "Enter only an integer, try again: ";
+                            getline(cin, UserInput);
+                        }
+                            
+                            if (!Integerv && !notInteger)
+                            {
+                                Flag = false;
+                            }
+        } return atoi(UserInput.c_str());
     }
 
     char *getcstr(const char *prompt, istream &istr, char delimiter)
     {
         char *cstr;
-        string str;
-        if (prompt != nullptr)
-            cout << prompt;
 
-        getline(istr, str);
-        cstr = new char[str.length() + 1];
-        strcpy(cstr, str.c_str());
+        string tmp;
+
+        if (prompt != nullptr)
+        {
+            cout << prompt;
+        }
+
+        getline(istr, tmp);
+
+        cstr = new char[tmp.length() + 1];
+
+        strcpy(cstr, tmp.c_str());
+        
         return cstr;
     }
 
-    int getTime()
+    int getInt(int min, int max, const char *prompt, const char *errorMessage, bool showRangeAtError)
     {
-        int mins{-1};
-        if (debug)
+        bool flag{true};
+        int inputFromUser = getInt(prompt);
+
+        while (flag)
         {
-            Time t(0);
-            cout << "Enter current time: ";
-            do
+            if (inputFromUser <= max && inputFromUser >= min)
             {
-                cin.clear();
-                cin >> t;
-                if (!cin)
-                {
-                    cout << "Invlid time, try agian (HH:MM): ";
-                    cin.clear();
-                }
+                flag = false;
+            }
                 else
                 {
-                    mins = int(t);
-                }
-                cin.ignore(1000, '\n');
-            } while (mins < 0);
-        }
-        else
-        {
-            time_t t = time(NULL);
-            tm lt = *localtime(&t);
-            mins = lt.tm_hour * 60 + lt.tm_min;
-        }
-        return mins;
+                    if (errorMessage != nullptr)
+                    {
+                        cout << errorMessage;
+                    }
+
+                    else
+                    {
+                        getInt(prompt);
+                    }
+
+                    if (showRangeAtError)
+                    {
+                        cout << "[" << min << " <= value <= " << max << "]: ";
+                    }
+
+                inputFromUser = getInt();
+            }
+        } return inputFromUser;
     }
 }
